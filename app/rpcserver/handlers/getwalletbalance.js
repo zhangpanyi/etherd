abff3d127a1a665b0955a64de300589d2150d25b
zhangpanyi <zhangpanyi@live.com>
2019-05-09 12:57:24 +0800

    update

--- app/rpcserver/handlers/getwalletbalance.js
+++ app/rpcserver/handlers/getwalletbalance.js
@@ -1,6 +1,6 @@
 const Utils = require('./utils/utils');
 const validator = require('validator');
-const future = require('../../common/future');
+const BigNumber = require('bignumber.js');
 
 module.exports = async function(ethereum, req, callback) {
     // 校验参数
@@ -21,6 +21,19 @@ module.exports = async function(ethereum, req, callback) {
         return;
     }
 
+    // 获取钱包余额
+    var balances = [];
+    const zero = new BigNumber(0, 10);
+    const dict = ethereum.getWalletBalances(rule[0].value);
+    const keys = Object.keys(dict);
+    for (index in keys) {
+        const address = keys[index];
+        const balance = new BigNumber(dict[address], 10);
+        if (balance.comparedTo(zero) > 0) {
+            balances.push({'address': address, 'balance': dict[address]});
+        }
+    }
+
     // 计算钱包余额
-    callback(undefined, ethereum.getWalletBalances(rule[0].value));
+    callback(undefined, balances);
 }
