f0f240b7bdd7d65a0273a3ea6375b810a0a86c4f
zhangpanyi <zhangpanyi@live.com>
2019-06-11 11:35:31 +0800

    update

--- app/ethereum.js
+++ app/ethereum.js
@@ -282,9 +282,8 @@ class Ethereum {
         }
 
         // 发送ERC20代币
-        let contract = this.getContract(token);
         [error, hash] = await future(this._transfer.sendERC20Token(
-            symbol, contract, decimals, from, to, amount, privateKey));
+            symbol, token.contractAddress, decimals, from, to, amount, privateKey));
         if (error != null) {
             throw error;
         }
